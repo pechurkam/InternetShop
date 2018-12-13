cc33f39e5277f2ebf7b0a9a879a694ae83feca83
Milena <MilenaPechura@gmail.com>
2018-12-13 18:25:45 +0200

    changed img folder(2)

--- src/modules/productInCart.js
+++ src/modules/productInCart.js
@@ -12,13 +12,13 @@ let _productInCart = ({
 </div>
 <div class="row" style="padding-bottom: 10px; padding-top: 10px">
     <div class="decreaseButton col-xs-2 col-sm-2 col-md-2 col-lg-2">
-        <img src="../img/left-arrow.png" width="15" height="auto" alt="right">
+        <img src="img/left-arrow.png" width="15" height="auto" alt="right">
     </div>
     <div class="productAmount col-xs-2 col-sm-2 col-md-2 col-lg-2">
         ${itemCount}
     </div>
     <div class="increaseButton col-xs-2 col-sm-2 col-md-2 col-lg-2">
-        <img src="../img/right-arrow.png" width="15" height="auto" alt="right">
+        <img src="img/right-arrow.png" width="15" height="auto" alt="right">
     </div>    
 </div></div>
 `));
